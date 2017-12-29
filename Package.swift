90f0406d0f3c3ef83c7fb1ddbb503112558992cc
yuta24 <yuta24@bivre.com>
2017-12-29 15:45:37 +0900

    Tweak

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RealmGen",
-            dependencies: ["RealmGenKit", "SourceKittenFramework", "Stencil"]),
+            dependencies: ["SourceKittenFramework", "Stencil"]),
         .target(
             name: "RealmGenKit",
             dependencies: []),
